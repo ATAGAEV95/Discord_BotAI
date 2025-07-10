115e3b8a0759b9536275193003e51083d4cf5479
ATAGAEV <preator-man@mail.ru>
2025-07-10 17:44:45 +0300

    2025-07-10

--- app/handlers.py
+++ app/handlers.py
@@ -8,15 +8,16 @@ from openai import AsyncOpenAI
 load_dotenv()
 
 AI_TOKEN = os.getenv('AI_TOKEN')
+AI_TOKEN1 = os.getenv('AI_TOKEN1')
 
 SYSTEM_PROMPT = """
 Ты — грубый Discord бот. Твои задачи:
 1. Общаться с пользователями в Discord-чатах
-2. Использовать неформальный стиль общения
+2. Использовать неформальный стиль общения(использовать мат, но по разному)
 3. Избегать сложных технических терминов, если пользователь не попросит
 4. Отвечать кратко (1-2 предложения) для удобства чтения в чате
-5. Если пишет пользователь с user_id - serious_vlad, то он админ канала и проявить к нему особое отношение.
-6. Если пишет пользователь с user_id - rikka71, то обращать внимание на его сильные скиллы в шутерах.
+5. Если пишет пользователь с user_id - serious_vlad, то он админ канала
+6. Если пишет пользователь с user_id - rikka71, то обращать внимание на его сильные скиллы в шутерах
 
 Текущая платформа: Discord
 """
@@ -24,7 +25,9 @@ SYSTEM_PROMPT = """
 
 client = AsyncOpenAI(
     api_key=AI_TOKEN,
+    # api_key=AI_TOKEN1,
     base_url="https://api.proxyapi.ru/openai/v1",
+    # base_url="https://api.aitunnel.ru/v1/",
 )
 
 user_history = {}
@@ -81,8 +84,8 @@ async def ai_generate(text: str, user_id: int):
 
     try:
         completion = await client.chat.completions.create(
-            # model="gpt-4.1-nano",
-            # model = "gpt-4o-mini",
+            # model="grok-3-mini-beta",
+            # model = "gemini-2.5-flash-preview-05-20-thinking",
             # model="gpt-4.1-mini",
             model="gpt-4.1",
             messages=messages,
@@ -99,7 +102,7 @@ async def ai_generate(text: str, user_id: int):
         # print(len(messages))
         #return cleaned_response_text + '\n' + '_______________' + '\n' + f'Сообщений: {len(messages)}' + '\n' + \
         #    f'Длина: {len(str(messages))}' + '\n' + f'Ответ: {len(cleaned_response_text)}'
-        total_token_count = sum(count_tokens(msg.get("content", "")) for msg in messages)
+        # total_token_count = sum(count_tokens(msg.get("content", "")) for msg in messages)
 
         return cleaned_response_text
 
