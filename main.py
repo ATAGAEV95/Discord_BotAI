50abbb345c34bf130cf021df9f20c16b2797e01c
atagaev95 <predator-man@mail.ru>
2026-03-05 19:08:45 +0300

    telegram notifier disable

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ intents.message_content = True
 
 
 # Настройки функций
-ENABLE_TELEGRAM_NOTIFIER = True  # Включить/выключить уведомления в Telegram
+ENABLE_TELEGRAM_NOTIFIER = False  # Включить/выключить уведомления в Telegram
 ENABLE_WEATHER = False  # Включить/выключить поиск погоды (нужен API ключ)
 ENABLE_SEARCH = False   # Включить/выключить поиск в интернете (нужен API ключ)
 
